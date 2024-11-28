f1fe853526c5c7fd1c580534f5c235fc2811539f
esterzane <zaneestere@gmail.com>
2024-11-28 19:43:03 +0000

    js started

---
+++ script.js
@@ -0,0 +1,7 @@
+const cityInput = document.querySelector (".city-input");
+const searchButton = document.querySelector (".search-btn");
+
+const getCityCoordinates = () => {
+    const cityName =
+}
+searchButton.addEventListener ("click", getCityCoordinates);
