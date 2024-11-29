8bbaf94cd5dc418b166afd0b675fddfae9e3a0bf
esterzane <zaneestere@gmail.com>
2024-11-29 12:35:24 +0000

    weather API Javascript (Get city Coordinates and Filter Forecast Data)

--- script.js
+++ script.js
@@ -1,7 +1,54 @@
-const cityInput = document.querySelector (".city-input");
-const searchButton = document.querySelector (".search-btn");
+const cityInput = document.querySelector(".city-input");
+const searchButton = document.querySelector(".search-btn");
+const API_KEY = "22038a70491f6e8a843fa6f83a67e41a"; // API key for OpenWeatherMap API
+
+const createWeatherCard = (weatherItem) => {
+
+    return " <li class="card">
+        <h3>(${ weatherItem.dt_text.split(" ")[0] })</h3>
+        <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png"/>
+        <h4> Temp: ${(weatherItem.main.temp - 273.1).toFixed(2)}°C</h4>
+        <h4>Wind: ${weatherItem.wind.speed} km/h</h4>
+         <h4>Humidity: ${weatherItem.main.humidity}%</h4>
+       </li > ";
+}
+const getWeatherDetails = (cityName, lat, lon) => {
+    const WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/forecast/?lat=${lat}&lon=${lon}&appid=${API_KEY}";
+
+    fetch(WEATHER_API_URL).then(res => res.json()).then(data = {
+
+        // Filter the forecast to get only one forecast per day
+        const uniqueForecastDays = [];
+        const fiveDaysForecast = data.list.filter(forecast => {
+            const forecastDate = new Date(forecast.dt_txt).getDate();
+            if (!uniqueForecastDays.includes(forecastDate)) {
+                return uniqueForeCastDays.push(forecastDate);
+            }
+        });
+        console.log(fiveDaysForecast);
+        fiveDaysForecast.forEach(weatherItem => {
+            createWeatherCard();
+
+        });
+    }).catch(() => {
+        alert("An error occured while fetching the weather forecast!");
+    });
+}
 
 const getCityCoordinates = () => {
-    const cityName =
+    const cityName = cityInput.value.trim();
+    if (!cityName) return;
+    const GEOCODING_API_URL = "http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}"
+
+    // Get entered city coordinates (latitude, longitude, and name) from the API response 
+
+    fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
+        if (!data.length) return alert("No coordinates found for ${cityName}");
+        const { name, lat, lon } = data[0];
+        getWeatherDetails(name, lat, lon)
+    }).catch(() => {
+        alert("An error occured while fetching the coordinates!");
+    });
 }
-searchButton.addEventListener ("click", getCityCoordinates);
+
+searchButton.addEventListener("click", getCityCoordinates); 
