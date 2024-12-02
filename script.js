3eb95c82733c474ce9cff743332868bcfaeb8618
esterzane <zaneestere@gmail.com>
2024-12-02 12:20:16 +0000

    js current card integrated without final funcionality

--- script.js
+++ script.js
@@ -1,52 +1,68 @@
 const cityInput = document.querySelector(".city-input");
 const searchButton = document.querySelector(".search-btn");
-const currentWeatherDiv = document.quarySelector (".current-weather");
-const weatherCardsDiv = document.quarySelector (".weather-cards");
-const API_KEY = "22038a70491f6e8a843fa6f83a67e41a"; // 'PI key for OpenWeatherMap API
+const currentWeatherDiv = document.quarySelector(".current-weather");
+const weatherCardsDiv = document.quarySelector(".weather-cards");
+const API_KEY = "22038a70491f6e8a843fa6f83a67e41a"; // API key for OpenWeatherMap API
 
-const createWeatherCard = (weatherItem) => {
-    return `<li class="card">
-            <h3>${weatherItem.dt_text.split(" ")[0]}</h3>
-            <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="Weather icon"/>
-            <h4>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h4>
-            <h4>Wind: ${weatherItem.wind.speed} km/h</h4>
-            <h4>Humidity: ${weatherItem.main.humidity}%</h4>
-            </li>`;
-};
+const createWeatherCard = (cityName, weatherItem, index) => {
+    if (index === 0) {
+        return ' <div class="details">' 
+            <h2>${cityName}//</h2>
+            <h4>Temperature: ____ °C</h4>
+            <h4> Wind: __ km/h</h4>
+            <h4> Humidity: __ %</h4>
+        </div>
+        <div class="icon">
+             <img src="https://openweathermap.org/img/wn/10d@4x.png"/>
+             <h4>Moderate rain</h4>
+        </div>';
+    } else {
+        return `<li class="card">
+        <h3>${weatherItem.dt_text.split(" ")[0]}</h3>
+        <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="Weather icon"/>
+        <h4>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h4>
+        <h4>Wind: ${weatherItem.wind.speed} km/h</h4>
+        <h4>Humidity: ${weatherItem.main.humidity}%</h4>
+        </li>`;
+
+    }
+
+}
 
 const getWeatherDetails = (cityName, lat, lon) => {
     const WEATHER_API_URL = "http'//api.openweathermap.org/data/2.5/forecast/?lat=${lat}&lon=${lon}&appid=${API_KEY}";
 
     fetch(WEATHER_API_URL)
-    .then(res => res.json())
-    .then(data => {
-        // Filter the forecast to get only one forecast per day
-        const uniqueForecastDays = [];
-        const fiveDaysForecast = data.list.filter(forecast => {
-            const forecastDate = new Date(forecast.dt_txt).getDate();
-            if (!uniqueForecastDays.includes(forecastDate)) {
-               return uniqueForecastDays.push(forecastDate);
-            }
-        });
+        .then(res => res.json())
+        .then(data => {
+            // Filter the forecast to get only one forecast per day
+            const uniqueForecastDays = [];
+            const fiveDaysForecast = data.list.filter(forecast => {
+                const forecastDate = new Date(forecast.dt_txt).getDate();
+                if (!uniqueForecastDays.includes(forecastDate)) {
+                    return uniqueForecastDays.push(forecastDate);
+                }
+            });
 
-        // Clearing previous weather data 
+            // Clearing previous weather data 
 
-        cityInput.value = ""; 
-        currentWeatherDiv.innerHTML = "";
-        weatherCardsDiv.innerHTML = "";
-        console.log(fiveDaysForecast);
-        fiveDaysForecast.forEach(weatherItem, item=> {
-            if (item === 0) {
-                weatherCardsDiv.insertAdjacentHTML("beforeend",  createWeatherCard());
-             }
-             else { 
-                weatherCardsDiv.insertAdjacentHTML("beforeend",  createWeatherCard());
-             }   
+            cityInput.value = "";
+            currentWeatherDiv.innerHTML = "";
+            weatherCardsDiv.innerHTML = "";
+            console.log(fiveDaysForecast);
+            fiveDaysForecast.forEach((weatherItem, index) => {
+                if (index === 0) {
+                    currentWeatherDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
+                }
+                else {
+                    weatherCardsDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
+                }
+            });
+
+        }).catch(() => {
+            alert("An error occured while fetching the weather forecast!");
         });
-        .catch(() => {
-        alert("An error occured while fetching the weather forecast!");
-    });
-}; 
+};
 
 const getCityCoordinates = () => {
     const cityName = cityInput.value.trim();
